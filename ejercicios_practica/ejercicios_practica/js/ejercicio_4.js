5eaeaeb267b2b0d63ef3f2df9cb081cd1454321f
Pamela544 <112887738+Pamela544@users.noreply.github.com>
2023-05-08 01:29:32 -0300

    Add files via upload

---
+++ ejercicios_practica/ejercicios_practica/js/ejercicio_4.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 4. Realice un acumulador utilizando una variable, debe acumular en 5 lineas
+//    de código el numero 100, comenzando desde el 1.
+
+
+let acumulador = 1;
+acumulador= acumulador + 100;
+
+acumulador= acumulador + 100;
+
+acumulador= acumulador + 100;
+
+acumulador= acumulador + 100;
+acumulador= acumulador + 100;
+
+//Imprimimos en consola:
+console.log("El resultado del programa es: " + acumulador); 
+
---
+++ ejercicios_practica/ejercicios_practica/js/ejercicio_5.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+ 
+ //-------------------------------------------------------------------
+// 5. Utilizar una sola variable para almacenar texto o numeros, 
+//    e imprimir en consola varias veces para construir la frase:
+//    El numero
+//    7
+//    me trae mas suerte que el numero 
+//    13
+
+
+let textoNumero = "El numero ";
+textoNumero = textoNumero + "7";
+textoNumero = textoNumero + "me trae mas suerte que el numero";
+textoNumero = textoNumero + "13";
+ console.log(textoNumero);
